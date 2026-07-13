b715e4b99efaf48f289b423192da275ba07c0066
mari-no <marin.nosova@gmail.com>
2026-07-12 22:02:24 -0500

    Assignment 2 Python

---
+++ assignment2/diary.py
@@ -0,0 +1,36 @@
+    ##Task 1
+import traceback
+
+
+try:
+    with open('diary.txt', 'a') as diary:
+        first = True
+        record=""
+        while  record != "done for now":
+            if first==True:
+                record = input("What happened today? ")
+                first=False
+                diary.write(record+"\n")
+            else:
+                record = input("What else? ")
+                diary.write(record+"\n")
+
+
+        
+
+
+
+except Exception as e:
+   
+    trace_back = traceback.extract_tb(e.__traceback__)
+    stack_trace = list()
+    for trace in trace_back:
+      stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+    print(f"Exception type: {type(e).__name__}")
+    message = str(e)
+    if message:
+      print(f"Exception message: {message}")
+    print(f"Stack trace: {stack_trace}")
+    print(f"An exception occurred: {e}")
+else:
+    print("The file was read ok.")
